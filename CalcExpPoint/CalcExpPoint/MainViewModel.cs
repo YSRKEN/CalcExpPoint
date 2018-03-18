ac67e86c2ebffd959cf39457cc474727e2164347
YSR <ysr.ken@gmail.com>
2018-03-19 07:23:45 +0900

    ViewModelをReactivePropertyで実装した

---
+++ CalcExpPoint/CalcExpPoint/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Reactive.Bindings;
+using System;
+using System.ComponentModel;
+
+namespace CalcExpPoint
+{
+	class MainViewModel : INotifyPropertyChanged
+	{
+		public ReactiveProperty<int> NowLevel { get; } = new ReactiveProperty<int>(1);
+		public ReactiveProperty<int> EndLevel { get; } = new ReactiveProperty<int>(165);
+		public ReactiveProperty<string> OutputMessage { get; } = new ReactiveProperty<string>("");
+		public ReactiveCommand CalcCommand { get; } = new ReactiveCommand();
+		//
+		public MainViewModel()
+		{
+			CalcCommand.Subscribe(_ => {
+				OutputMessage.Value = "テスト";
+			});
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
